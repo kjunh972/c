219bb7842c95df6f9df40f1e170e1c4b576d128e
김준형 <kjunh972@gmail.com>
2017-04-10 23:55:41 +0900

    5의 배수 구하기

---
+++ 5의배수구하기.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+void main() {
+
+	int i = 0, sum = 0, n = 0;
+
+	printf("n의 값을 입력 하세요 : ");
+	scanf("%d", &n);
+
+	while (i < n) {
+
+		i = i + 1;
+
+		if (i % 5 == 0) {
+
+			sum = sum + i;
+		}
+	}
+
+	printf("합 : %d \n", sum);
+
+}
