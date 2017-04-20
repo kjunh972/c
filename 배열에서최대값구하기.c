fd9d25b065a973110a46e2a9b5ff10bbaaf83693
김준형 <kjunh972@gmail.com>
2017-04-20 20:55:51 +0900

    배열에서 최대값 구하기

---
+++ 배열에서최대값구하기.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+
+	int i = 0, max = 0;
+	int arr[4] = { 1,2,3,4 };
+	max = arr[0];
+
+	for (i = 1; i <= 3; i++)
+	{
+
+		if (max < arr[i])
+			max = arr[i];
+	}
+	printf("�ִ밪 : %d", max);
+}
