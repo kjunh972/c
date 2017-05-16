3b0e433f4bc35eb49cadbbd44700094eaa525784
김준형 <kjunh972@gmail.com>
2017-05-16 23:57:00 +0900

    입력한 횟수대로 반복하기

---
+++ 입력한횟수대로반복하기.c
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+void main()
+{
+
+	int i = 0, count = 0;
+	printf("반복할 횟수를 입력해주십시오 : ");
+	scanf("%d", &count);
+
+	while (1)
+	{
+
+		printf("%d\n", i);
+		if (i == count)
+			break;
+
+		i++;
+
+	}
+
+}
