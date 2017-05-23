e435c357832fd3082c4f3bc6efd4e68be23e69c6
김준형 <kjunh972@gmail.com>
2017-05-23 21:49:39 +0900

    더하기 빼기 번갈아가며 계산하기

---
+++ 더하기빼기번갈아가며계산하기.c
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+void main()
+{
+
+	int i = 0, sum = 0, n = 0, a = 1;
+
+	printf("숫자를 입력해주세요 : ");
+	scanf("%d", &n);
+
+	for (i = 1; i <= n; i++)
+	{
+
+		sum = sum + i*a;
+			a = -a;
+
+	}
+
+	printf("합 : %d\n", sum);
+
+}
