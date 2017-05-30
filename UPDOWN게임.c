703b8e7ad494ba07be4a309622fe250fbcf36962
김준형 <kjunh972@gmail.com>
2017-05-30 22:04:20 +0900

    UP DOWN 게임

---
+++ UPDOWN게임.c
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+void main()
+{
+
+	int answer = 0, input = 0;
+
+	printf("숫자를 하나 정하세요 : ");
+	scanf("%d", &answer);
+	printf("숫자를 맞쳐보세요 : ");
+	scanf("%d", &input);
+
+	while (answer != input)
+	{
+
+		if (answer > input)
+			printf("UP\n");
+
+		if (answer < input)
+			printf("Down\n");
+
+			printf("숫자를 맞쳐보세요 : ");
+			scanf("%d", &input);
+
+	}
+
+	printf("정답\n");
+
+}
