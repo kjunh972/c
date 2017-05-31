79e9cacaabec623deb6fc9b39a863cbc50bdd089
김준형 <kjunh972@gmail.com>
2017-05-31 23:08:34 +0900

    컴퓨터와 UP Down 게임하기

---
+++ 컴퓨터와UPDown게임하기.c
@@ -0,0 +1,31 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+void main()
+{
+
+	int answer, input = 0;
+
+	srand(time(NULL));
+	answer = rand() % 1000 + 1;
+
+	printf("숫자를 맞쳐보세요 : ");
+	scanf("%d", &input);
+
+	while (answer != input)
+	{
+
+		if (answer > input)
+			printf("UP\n");
+
+		if (answer < input)
+			printf("Down\n");
+
+		printf("숫자를 맞쳐보세요 : ");
+		scanf("%d", &input);
+
+	}
+
+	printf("정답\n");
+
+}
