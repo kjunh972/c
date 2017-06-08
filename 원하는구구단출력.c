81e6502690ca40ac538c29faaeba328522139f8a
김준형 <kjunh972@gmail.com>
2017-06-08 22:41:09 +0900

    원하는 구구단 출력하기

---
+++ 원하는구구단출력.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+void main()
+{
+
+	int n = 0, i = 0;
+	
+	printf("몇단을 출력할지 입력해주세요 : ");
+	scanf("%d", &n);
+
+	for (i = 1; i <= 9; i++)
+	{
+
+		printf("%d * %d = %d\n", n, i, n*i);
+
+	}
+}
