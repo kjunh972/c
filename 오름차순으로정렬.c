d3b248e050bf05be4052cb39b875c9abe73b516a
kimjunhyeong <kjunh972@naver.com>
2017-09-05 18:56:53 +0900

    오름차순으로 정렬

---
+++ 오름차순으로정렬.c
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+void main()
+{
+
+	int A[5] = { 30,50,90,70,60 };
+	int min = 0, a = 0, i = 0, pos = 0;
+
+	for (a = 0; a < 5; a++)
+	{
+
+		min = 100, pos = 0;
+		
+		for (i = 0; i < 5; i++)
+		{
+
+			if (A[i] < min)
+			{
+				min = A[i];
+				pos = i;
+
+			}
+		}
+
+		printf("%d ", min);
+		A[pos] = 100;
+	}
+}
