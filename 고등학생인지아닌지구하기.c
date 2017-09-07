2e6b0ba67ff258442af72f092fb1581c5e2bc1ae
kimjunhyeong <kjunh972@naver.com>
2017-09-07 21:12:59 +0900

    고등학생인지 아닌지 구하기

---
+++ 고등학생인지아닌지구하기.c
@@ -0,0 +1,31 @@
+﻿#include <stdio.h>
+int main()
+{
+
+	int age = 0;
+
+	printf("나이를 입력해주세요 : ");
+	scanf("%d", &age);
+
+	switch (age)
+	{
+	case 17:
+		printf("고등학생 1학년입니다.\n");
+		break;
+
+	case 18:
+		printf("고등학생 2학년입니다.\n");
+		break;
+
+	case 19:
+		printf("고등학생 3학년입니다.\n");
+		break;
+
+	default:
+		printf("고등학생이 아닙니다.\n");
+		break;
+
+	}
+
+	return 0;
+}
