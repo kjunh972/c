1124ccacb5eadab333977171673aa852df9b6120
kimjunhyeong <kjunh972@naver.com>
2017-09-10 23:07:22 +0900

    입력한 수의 역순으로 구구단 하기

---
+++ 입력한수의역순으로구구단하기.c
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+
+void main()
+{
+
+	int i = 0, n = 0;
+
+	printf("구구단을 할 수를 입력해주세요 : ");
+	scanf("%d", &n);
+
+
+		for (i = 9; 0<i<10; i--)
+		{
+			if (i == 0)
+			{
+
+				break;
+
+			}
+			
+			printf("%d X %d = %d \n", n, i, n*i);
+
+		}
+
+		return 0;
+}
