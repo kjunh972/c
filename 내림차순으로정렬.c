b155324649efc7fc30e66b32b48d78903bc0908a
kimjunhyeong <kjunh972@naver.com>
2017-09-11 23:31:19 +0900

    내림차순으로 정렬

---
+++ 내림차순으로정렬.c
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+int main()
+{
+
+	int A[5] = { 20,15,41,36,95 };
+	int min = 100, pos = 0, i = 0, a = 0;
+
+	for (a = 0; a < 5; a++)
+	{
+
+		min = 1, pos = 0;
+
+		for (i = 0; i < 5; i++)
+		{
+			if (A[i] > min)
+			{
+				min = A[i];
+					pos = i;
+			}
+		}
+		printf("%d ", min);
+			A[pos] = 1;
+	}
+}
