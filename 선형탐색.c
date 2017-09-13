e359f43c4f39f1bb4467c92c5ff4a336dcc4206c
kimjunhyeong <kjunh972@naver.com>
2017-09-13 22:52:39 +0900

    선형탐색

---
+++ 선형탐색.c
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+int main()
+{
+
+	int A[5] = { 10,20,30,40,50 };
+	int a = 0 , i = 0;
+	scanf("%d", &a);
+
+	for (i = 0; i < 5; i++)
+	{
+
+		if (A[i] == a)
+		{
+
+			printf("찾았습니다.");
+			break;
+
+		}
+
+		else if (A[i] != a)
+		{
+
+			printf("원하는 숫자가 없습니다.");
+			break;
+
+		}
+	}
+
+	return 0;
+}
