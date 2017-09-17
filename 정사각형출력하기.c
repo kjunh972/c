6287fc37208d0ef039eedc1a262565afdca8fc7e
kimjunhyeong <kjunh972@naver.com>
2017-09-17 23:32:38 +0900

    정사각형 출력하기

---
+++ 정사각형출력하기.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(void)
+{
+    int n;
+    int i, j;
+ 
+    printf("출력할 라인 수: ");
+    scanf_s("%d", &n);
+    
+    for (i = 0; i < n; i++)
+    {
+    	
+        for (j = 0; j < n; j++)
+        {
+        	
+            printf("* ");
+            
+        }
+        printf("\n");
+        
+    }
+    
+    return 0;
+} 
