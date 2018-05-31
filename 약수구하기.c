808e44a90acd4847a7cdf0c1b50dea53167a07f3
kimjunhyeong <kjunh972@naver.com>
2018-05-31 21:58:42 +0900

    약수구하기

---
+++ 약수구하기.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main()
+
+{
+
+	int num = 0, num1 = 0;
+
+ 
+
+	printf("숫자입력 : ");
+
+	scanf_s("%d", &num1);
+
+ printf("%d의 약수 : ",num1); 
+
+	while (num<=num1)
+
+	{
+
+		num += 1;
+		if (num1%num==0)
+
+		{
+
+			printf("%-3d", num);
+
+		}
+
+	}
+
+	printf("\n");
+
+ 
+
+	return 0;
+
+}
